e6b1c09dc3077ce5d216a5fbd3e6157ed72487f5
Jamila Aliyeva and Will Quanstrom <will@>
2019-11-18 15:23:56 -0800

    it will add UI

--- src/main.js
+++ src/main.js
@@ -3,3 +3,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from 'jquery';
 import { Tomagotchi } from './tomagotchi.js';
+
+$(document).ready(function(){
+  $('.tamaName').submit(function(event){
+    event.preventDefault();
+    $('#toHide').hide();
+    var name = $('#nameInput').val();
+    var tama = new Tomagotchi(name);
+    console.log(tama);
+  });
+});
--- src/tomagotchi.js
+++ src/tomagotchi.js
@@ -1,67 +1,67 @@
 export class Tomagotchi {
-  constructor(name,food,toilet) {
-  this.name = name,
-  this.food = 10,
-  this.toilet = 20,
-  this.sleep = false;
-  this.hungerInterval;
-  this.toiletInterval;
-  this.playInterval;
-  this.play = 10,
-  this.sleepLevel = 30,
-};
+  constructor(name,food,toilet,sleep,play,sleepLevel) {
+    this.name = name,
+    this.food = 10,
+    this.toilet = 20,
+    this.sleep = false;
+    this.hungerInterval;
+    this.toiletInterval;
+    this.playInterval;
+    this.play = 10,
+    this.sleepLevel = 30;
+  }
 
-setToilet(){
-  this.toiletInterval = setInterval(() => {
-    this.toilet--;
-  }, 1000);
+  setToilet(){
+    this.toiletInterval = setInterval(() => {
+      this.toilet--;
+    }, 1000);
     if(this.toilet == 0){
-    return 'Your tomagotchi pooped!';
+      return 'Your tomagotchi pooped!';
+    }
   }
-}
-hungryGotchi () {
-  if (this.food <= 0) {
-    return  true;
-  } else {
-    return false;
+  hungryGotchi () {
+    if (this.food <= 0) {
+      return  true;
+    } else {
+      return false;
+    }
   }
-}
-feedSnack()  {
-  this.food += 2;
-  this.
-}
-feedMeal() {
-  this.food += 5;
-}
-sickGotchi (){
-  if (this.food >= 11) {
-    return "Your tomagotchi is sick: feed medicine"
+
+  feedSnack()  {
+    this.food += 2;
   }
-}
-feedMedicine() {
-  this.food -= 3;
-}
+  feedMeal() {
+    this.food += 5;
+  }
+  sickGotchi (){
+    if (this.food >= 11) {
+      return "Your tomagotchi is sick: feed medicine";
+    }
+  }
+  feedMedicine() {
+    this.food -= 3;
+  }
   setHunger() {
-  this.hungerInterval =  setInterval(() => {
+    this.hungerInterval =  setInterval(() => {
       this.food--;
     }, 1000);
 
+  }
+  sleepGotchi(){
+    clearInterval(this.hungerInterval);
+    clearInterval(this.toiletInterval);
+  }
+  setPlay(){
+    this.playInterval = setInterval(() => {
+      this.play--;
+    }, 2000);
+  }
+  playGotchi(){
+    this.play += 4;
+  }
+  setSleep(){
+    setInterval(() => {
+      this.sleepLevel--;
+    }, 1000);
+  }
 }
-sleepGotchi(){
-  clearInterval(this.hungerInterval);
-  clearInterval(this.toiletInterval);
-}
-setPlay(){
-  this.playInterval = setInterval(() => {
-    this.play--;
-  }, 2000);
-}
-playGotchi(){
-  this.play += 4;
-}
-setSleep(){
-  setInterval(() => {
-    this.sleepLevel--;
-  }, 1000);
-}
-};
