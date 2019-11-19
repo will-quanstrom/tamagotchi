f479f3a0a5b42dacdfc5c061c88d9606f8d6f10f
Jamila Aliyeva and Will Quanstrom <jamila@>
2019-11-19 14:08:17 -0800

    add a functional gif to webpage

--- src/main.js
+++ src/main.js
@@ -4,12 +4,31 @@ import './styles.css';
 import $ from 'jquery';
 import { Tomagotchi } from './tomagotchi.js';
 
+
 $(document).ready(function(){
   $('.tamaName').submit(function(event){
     event.preventDefault();
     $('#toHide').hide();
     var name = $('#nameInput').val();
     var tama = new Tomagotchi(name);
-    console.log(tama);
+      $('#tamaName').text(`Meet your new best friend, ${name}!`)
+
+let request = new XMLHttpRequest();
+const url = 'https://api.giphy.com/v1/gifs/search?api_key=eA5QrPG0kgbvymKiUi3pJqKZ0ZY54uIB&q=tamagotchi'
+
+request.onreadystatechange = function() {
+      if (this.readyState === 4 && this.status === 200) {
+        const response = JSON.parse(this.responseText);
+        getElements(response);
+      }
+    }
+
+    request.open("GET", url, true);
+    console.log(request.open);
+    request.send();
+
+    const getElements = function(response) {
+      document.getElementById('gif').src = response.data[0].images.downsized_large.url;
+    }
   });
 });
